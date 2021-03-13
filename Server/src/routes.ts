45871d03b9b41da488f7a3776bc2cae47e3be50e
Igor <iljavaroni@hotmail.com>
2021-03-13 02:32:52 -0300

    correções

--- Server/src/routes.ts
+++ Server/src/routes.ts
@@ -1,9 +1,50 @@
-import express from 'express';
+import express, { response } from 'express';
+import { request } from 'node:http';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
-    return response.json({message:'Hello Word'});
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+    
+    const serializedItems = items.map(item => { 
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+    
+    return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+    });
+
+
+
+
+    return response.json({success: true})
 });
 
 export default routes;
