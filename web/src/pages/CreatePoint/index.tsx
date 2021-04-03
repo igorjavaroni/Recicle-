c283238eb631fcfb4a2d58336f234fbdee3b74c3
GPiovesan <bieldeluna@gmail.com>
2021-04-03 00:18:17 -0300

    Criação do projeto react

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,120 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { TileLayer, Marker, MapContainer } from 'react-leaflet';
+import './styles.css';
+import logo from '../../assets/logo.svg';
+import api from '../../services/api';
+
+const CreatePoint = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        })
+    }, []);
+
+    return (
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+                
+                <Link to="/">
+                    <FiArrowLeft/>
+                    Voltar para home
+                </Link>
+            </header>
+
+            <form>
+                <h1>Cadastro do <br/> ponto de coleta</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input 
+                            type="text"
+                            name="name"
+                            id="name"
+                        />
+                    </div>
+
+                    <div className="field-group">
+                    <div className="field">
+                        <label htmlFor="email">E-mail</label>
+                        <input 
+                            type="email"
+                            name="email"
+                            id="email"
+                        />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="whatsapp">Whatsapp</label>
+                        <input 
+                            type="text"
+                            name="whatsapp"
+                            id="nwhatsappame"
+                        />
+                    </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
+                    </legend>
+
+                    <MapContainer center={[-27.2092052, -49.6401092]} zoom={15}>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-27.2092052, -49.6401092]} />
+                    </MapContainer>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Ítens de coleta</h2>
+                        <span>Selecione um ou mais itens abaixo</span>
+                    </legend>
+
+                    <ul className="items-grid">
+                        {items.map(item => (
+                            <li>
+                                <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo"/>
+                                <span>Óleo de Cozinha</span>
+                            </li>
+                        ))}                        
+                    </ul>
+                </fieldset>
+
+                <button type="submit">
+                    Cadastrar ponto de coleta
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default CreatePoint;
